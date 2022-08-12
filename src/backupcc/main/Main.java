6d3a6006e70ed8d2dda2598d170479b3bdb713ab
Pedro Reis <ph3dror3is@gmail.com>
2022-08-12 05:38:42 -0300

    Primeiro commit

---
+++ src/backupcc/main/Main.java
@@ -0,0 +1,20 @@
+package backupcc.main;
+
+/**
+ * Ponto de entrada para execucao da aplicacao.
+ * 
+ * @author "Pedro Reis"
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * @param args Parametros de linha de comando (nao usado)
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }//main()
+    
+}//classe Main
